be88dc8c92557d7068b6b6297f4cc6654b562e8d
Ryan Ruenroeng <rruenroeng@gmail.com>
2022-03-06 10:56:55 -0600

    Try userVideoConfig hook.

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,20 @@
+import { useVideoConfig } from "remotion";
+
 export const MyComposition = () => {
-	return null;
+	const {fps, durationInFrames, width, height } = useVideoConfig();
+	return (
+	<div
+		style= {{
+			flex: 1,
+			textAlign: 'center',
+			fontSize: '7em',
+			background:'black',
+			color: 'red',
+		}}
+		>
+			Hi Mom!
+			I made a {width}x{height}px video that is 
+			{ durationInFrames/fps} seconds long.
+		</div>
+	);
 };
